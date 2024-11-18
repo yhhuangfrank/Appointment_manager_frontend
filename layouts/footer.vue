07c2fba1cb420032bc1db5f45d8b353e36220122
FrankHuang <frank.huang@kooppi.com>
2024-11-18 15:33:47 +0800

    feat: add layouts

---
+++ layouts/footer.vue
@@ -0,0 +1,3 @@
+<template>
+  <div>Frank Huang 2024</div>
+</template>
---
+++ layouts/header.vue
@@ -0,0 +1,7 @@
+<template>
+  <nav class="navbar bg-body-tertiary">
+    <div class="container-fluid">
+      <span class="navbar-brand mb-0 h1">Navbar</span>
+    </div>
+  </nav>
+</template>
