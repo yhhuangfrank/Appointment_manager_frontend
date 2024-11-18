0ba8a2fd576be253dcbbf960a065efaaadbe5fea
FrankHuang <frank.huang@kooppi.com>
2024-11-18 10:28:51 +0800

    project init

---
+++ app.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
+  </div>
+  <el-button @click="ElMessage('hello')">button</el-button>
+  <ElButton :icon="ElIconEditPen" type="success">button</ElButton>
+  <LazyElButton type="warning">lazy button</LazyElButton>
+</template>
